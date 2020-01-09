afa95c76321d6987c1aced6a691744d4782049ad
Z8pn <kryptoholyx@googlemail.com>
2020-01-09 10:29:37 +0100

    Player Commands

--- server/logic/player_property.js
+++ server/logic/player_property.js
@@ -1,10 +1,29 @@
 var Animations = require("../libs/animations.js");
 var Interaction = require("../interaction");
-var Vehicle = require("../models/vehicle.js").vehicle;
-mp.events.addCommand("sveh", (player, fullText, ...args) => {
+var Vehicle = require("../models/vehicle/vehicle.js").vehicle;
+mp.events.addCommand("r", (player, fullText, ...args) => {
+	let veh = player.vehicle;
+	console.log(veh);
+	veh.interface.respawn();
+});
+mp.events.addCommand("rgb", (player, fullText, ...args) => {
+	let veh = player.vehicle;
+	let [r, g, b] = args
+	veh.interface.setTune = {
+		"rgb": {
+			r: 255,
+			g: 100,
+			b: 200
+		}
+	};
+	console.log(veh);
+	veh.interface.reloadTunings();
+	//veh.interface.respawn();
+});
+mp.events.addCommand("veh", (player, fullText, ...args) => {
 	let pos = player.position;
 	let model = args[0];
-	var veh = new Vehicle(player, 0, {
+	var veh = new Vehicle(player, null, {
 		"model": model,
 		"x": pos.x,
 		"y": pos.y,
