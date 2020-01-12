10800b45e610727cd0a513bfb5670ecff0c69b6c
Z8pn <kryptoholyx@googlemail.com>
2020-01-12 04:01:43 +0100

    db

--- server/database/index.js
+++ server/database/index.js
@@ -1,29 +1,32 @@
-const {Sequelize,Model,DataTypes} = require('sequelize');
-const Op = Sequelize.Op;
+const {
+	Sequelize,
+	Model,
+	DataTypes
+} = require('sequelize');
 
-const sequelize = new Sequelize('reallife', 'root', '', {
+// pass TMmzs9oo9DL6Y9Fm;
+const Op = Sequelize.Op;
+const database_name = "reallife";
+const sequelize = new Sequelize(database_name, 'root', 'TMmzs9oo9DL6Y9Fm', {
 	host: '127.0.0.1',
-	dialect: 'mysql'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-},{
+	dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+}, {
 	timestamps: false
 });
-
-
+sequelize.query("CREATE DATABASE `" + database_name + "`;").then(data => {
+	console.log("created");
+});
 var account = require("./account.js")(sequelize);
 var vehicle = require("./vehicle.js")(sequelize);
-
-
-
 sequelize.authenticate().then(() => {
 	sequelize.sync().then(() => {
 		console.log('Connection has been established successfully.');
 	}).catch(err => {
-		console.error('Failed syncing Database',err);
+		console.error('Failed syncing Database', err);
 	});
 }).catch(err => {
 	console.error('Unable to connect to the database:', err);
 });
-
 module.exports = {
 	Op: Op,
 	account: account,
