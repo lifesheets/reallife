c1c93144381dee04610a7e62a6fbdc4d008b2368
Z8pn <kryptoholyx@googlemail.com>
2020-01-12 04:35:37 +0100

    db

--- server/database/index.js
+++ server/database/index.js
@@ -3,10 +3,26 @@ const {
 	Model,
 	DataTypes
 } = require('sequelize');
+var mysql = require('mysql');
+
 
 // pass TMmzs9oo9DL6Y9Fm;
 const Op = Sequelize.Op;
-const sequelize = new Sequelize("reallife", 'root', 'TMmzs9oo9DL6Y9Fm', {
+const sequelize = new Sequelize("reallife", 'root', '1234567890', {
+	host: '127.0.0.1',   //or 127.0.0.1
+  	dialect: 'mysql',
+  	logging: function () {},
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    dialectOptions: {
+        socketPath: "/var/run/mysqld/mysqld.sock"
+    },
+    define: {
+        paranoid: true
+    }
 }, {
 	timestamps: false
 });
