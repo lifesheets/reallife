9345fb996cf0a7745662600eb155bfd7f6272a83
Z8pn <kryptoholyx@googlemail.com>
2020-01-15 05:01:19 +0100

    house marker

--- client/index.js
+++ client/index.js
@@ -539,7 +539,7 @@ mp.events.add("cef:character:edit", (setClothing = false) => {
 var render_house = undefined;
 mp.events.add("render", () => {
 	if (render_house == undefined) return;
-	mp.game.graphics.drawMarker(6, render_house.x, render_house.y, render_house.z, 0, 0, 0, 0, 0, 0, 1, 1, 1, 36, 214, 42, 200, true, false, 2, false, "", "", false);
+	mp.game.graphics.drawMarker(28, render_house.x, render_house.y, render_house.z, 0, 0, 0, 0, 0, 0, 1, 1, 1, 36, 214, 42, 200, true, false, 2, false, "", "", false);
 });
 mp.events.add("server:estate:enablemarker", (data) => {
 	render_house = JSON.parse(data);
