007e9e42f5f6a8e9701c4fb93947f4faf324ecb8
Alex <Z8pn@users.noreply.github.com>
2020-06-14 05:49:37 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,11 @@
-npm i watchify --save
+npm i -g watchify --save
 npm i ncp --save
 npm i node-watch --save
 npm i fs --save
 npm i async --save
 npm i mysql --save
 npm i uglifyify --save
+npm i sequelize --save
+npm i prepend-file --save
+npm i ncp --save
 pause
